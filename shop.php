748f78ed5d9a8b3b36a36b7baea082e9a86d5736
unknown <anle101201@gmail.com>
2021-11-05 09:26:33 +0700

    hoagaf-final

---
+++ shop.php
@@ -0,0 +1,538 @@
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+
+	<title>HOAGAF Shoes</title>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Downy Shoes Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- //custom-theme -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<link rel="stylesheet" href="css/shop.css" type="text/css" media="screen" property="" />
+	<link href="css/style7.css" rel="stylesheet" type="text/css" media="all" />
+	<!-- Owl-carousel-CSS -->
+	<link rel="stylesheet" type="text/css" href="css/jquery-ui1.css">
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<!-- font-awesome-icons -->
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<!-- //font-awesome-icons -->
+	<link href="//fonts.googleapis.com/css?family=Montserrat:100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800"
+	    rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800" rel="stylesheet">
+</head>
+
+<body>
+	<!-- banner -->
+<div class="banner_top innerpage" id="home">
+		<div class="wrapper_top_w3layouts">
+			<div class="header_agileits">
+				<div class="logo inner_page_log">
+					<h1><a class="navbar-brand" href="index.php"><span>HOAGAF</span> <i>Shoes</i></a></h1>
+				</div>
+				<?php
+					include 'include/sidebar.php'
+				?>
+				<div class="mobile-nav-button">
+					<button id="trigger-overlay" type="button"><i class="fa fa-bars" aria-hidden="true"></i></button>
+				</div>
+				<!-- cart details -->
+				<div class="top_nav_right">
+					<div class="shoecart shoecart2 cart cart box_1">
+						<form action="" method="post" class="last">
+							<input type="hidden" name="cmd" value="_cart">
+							<input type="hidden" name="display" value="1">
+							<button class="top_shoe_cart" type="submit" name="submit" value=""><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- //cart details -->
+		<!-- search -->
+		<div class="search_w3ls_agileinfo">
+			<div class="cd-main-header">
+				<ul class="cd-header-buttons">
+					<li><a class="cd-search-trigger" href="#cd-search"> <span></span></a></li>
+				</ul>
+			</div>
+			<div id="cd-search" class="cd-search">
+				<form action="#" method="post">
+					<input name="Search" type="search" placeholder="Click enter after typing...">
+				</form>
+			</div>
+		</div>
+		<!-- //search -->
+		<div class="clearfix"></div>
+		<!-- /banner_inner -->
+		<div class="services-breadcrumb_w3ls_agileinfo">
+			<div class="inner_breadcrumb_agileits_w3">
+
+				<ul class="short">
+					<li><a href="index.php">Trang Chủ</a><i>|</i></li>
+					<li>Shop</li>
+				</ul>
+			</div>
+		</div>
+		<!-- //banner_inner -->
+	</div>
+
+	<!-- //banner -->
+	<!-- top Products -->
+			
+	<div class="ads-grid_shop">
+		<div class="shop_inner_inf">
+			<!-- tittle heading -->
+
+			<!-- //tittle heading -->
+			<!-- product left -->
+			<div class="side-bar col-md-3">
+				<div class="search-hotel">
+					<h3 class="agileits-sear-head">Tìm Kiêm..</h3>
+					<form action="#" method="post">
+						<input type="search" placeholder="Product name..." name="search" required="">
+						<input type="submit" value=" ">
+					</form>
+				</div>
+				<!-- price range -->
+				<!-- <div class="range">
+					<h3 class="agileits-sear-head">Mưc giá</h3>
+					<ul class="dropdown-menu6">
+						<li>
+
+							<div id="slider-range"></div>
+							<input type="text" id="amount" style="border: 0; color: #ffffff; font-weight: normal;" />
+						</li>
+					</ul>
+				</div> -->
+				<!-- //price range -->
+				<!--preference -->
+
+				<div class="left-side">
+					<h3 class="agileits-sear-head">Hãng nổi bật</h3>
+					<ul>
+						<?php
+						$get_cate = $cat->show_cate();
+						if($get_cate){
+							while($result_cate = $get_cate->fetch_assoc()){
+						?>
+						<li>
+							
+							
+							<a href = "cateshop.php?cat_id=<?php echo $result_cate['cat_id']; ?>">
+							<i class="fas fa-archive"></i>
+							<span class="span"><?php echo $result_cate['cat_name']; ?></span>
+							
+							</a>
+						</li>
+							<?php
+							}
+						}
+							?>
+					</ul>
+				</div>
+				<div class="deal-leftmk left-side">
+					<h3 class="agileits-sear-head">Sản phẩm bán chạy</h3>
+					<div class="special-sec1">
+						<div class="col-xs-4 img-deals">
+							<img src="images/s17.jpg" alt="">
+						</div>
+						<div class="col-xs-8 img-deal1">
+							<h3>Vans Fire</h3>
+							<a href="single.php">500.000 VNĐ</a>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="special-sec1">
+						<div class="col-xs-4 img-deals">
+							<img src="images/s18.jpg" alt="">
+						</div>
+						<div class="col-xs-8 img-deal1">
+							<h3>Nike Jordan</h3>
+							<a href="single.php">1.000.000 VNĐ</a>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="special-sec1">
+						<div class="col-xs-4 img-deals">
+							<img src="images/s19.jpg" alt="">
+						</div>
+						<div class="col-xs-8 img-deal1">
+							<h3>Converse</h3>
+							<a href="single.php">750.000 VNĐ</a>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="special-sec1">
+						<div class="col-xs-4 img-deals">
+							<img src="images/s20.jpg" alt="">
+						</div>
+						<div class="col-xs-8 img-deal1">
+							<h3>Puma Carbon</h3>
+							<a href="single.php">800.000 VNĐ</a>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="special-sec1">
+						<div class="col-xs-4 img-deals">
+							<img src="images/s33.jpg" alt="">
+						</div>
+						<div class="col-xs-8 img-deal1">
+							<h3>HUMAN RACE</h3>
+							<a href="single.php">2.500.00 VNĐ</a>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<!-- //deals -->
+
+			</div>
+			<!-- //product left -->
+			<!-- product right -->
+			
+
+			<div class="left-ads-display col-md-9">
+				<div class="wrapper_top_shop">
+					<div class="col-md-6 shop_left">
+					<video  style="width:90%;"  autoplay >
+					
+    					<source  src="nike1.mp4">
+						
+					</video>
+					</div>
+					<div class="col-md-6 shop_right">
+						<video  style="width:90%" autoplay >
+    					<source src="puma2.mp4" >
+						</video>
+					</div>
+					<div class="clearfix"></div>
+					<!-- product-sec1 -->
+					<div class="product-sec1">
+						<!--/mens-->
+						<?php
+							$get_product = $product->show_product_user();
+							if($get_product){
+								while($result = $get_product->fetch_assoc()){
+						?>
+						<div class="col-md-4 product-men">
+							<div class="product-shoe-info shoe">
+								<div class="men-pro-item">
+									<div class="men-thumb-item">
+										<img src="admin/uploads/<?php echo $result['product_image']; ?>" alt="">
+										<div class="men-cart-pro">
+											<div class="inner-men-cart-pro">
+												<a href="single.php?pro_id=<?php echo $result['product_id']?>" class="link-product-add-cart">Xem Nhanh</a>
+											</div>
+										</div>
+										<span class="product-new-top">New</span>
+									</div>
+									<div class="item-info-product">
+										<h4>
+											<a href="single.php"><?php echo $result['product_name']; ?> </a>
+										</h4>
+										<div class="info-product-price">
+											<div class="grid_meta">
+												<div class="product_price">
+													<div class="grid-price ">
+														<span class="money "><?php echo number_format( $result['product_sale']).""."VNĐ"; ?><span><del><?php echo number_format( $result['product_price']).""."VNĐ"; ?></span></span>
+													</div>
+												</div>
+												<ul class="stars">
+													<li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
+													<li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
+													<li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
+													<li><a href="#"><i class="fa fa-star-half-o" aria-hidden="true"></i></a></li>
+													<li><a href="#"><i class="fa fa-star-o" aria-hidden="true"></i></a></li>
+												</ul>
+											</div>
+											<div class="shoe single-item hvr-outline-out">
+												<form action="#" method="post">
+													<input type="hidden" name="cmd" value="_cart">
+													<input type="hidden" name="add" value="1">
+													<input type="hidden" name="shoe_item" value="Bella Toes">
+													<input type="hidden" name="amount" value="675.00">
+													<button type="submit" class="shoe-cart pshoe-cart"><i class="fa fa-cart-plus" aria-hidden="true"></i></button>
+
+													<a href="#" data-toggle="modal" data-target="#myModal1"></a>
+												</form>
+
+											</div>
+										</div>
+										<div class="clearfix"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<?php
+								}
+							}
+							?>
+					
+						
+						<div>
+							<?php 
+							
+							?>
+						</div>
+					</div>
+						<!-- //womens -->
+						<!-- /mens -->
+						
+						
+						
+						
+
+					<!-- //product-sec1 -->
+					<div class="col-md-6 shop_left shp">
+					<video style="width:90%;"  autoplay >					
+    					<source  src="airmax.mp4">
+					</video>
+					</div>
+					<div class="col-md-6 shop_right shp">
+					<video style="width:90%;"  autoplay >					
+    					<source  src="giay.mp4">
+					</video>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+		<style type="text/css">
+				a.phantrang {
+				    border: 1px solid #ddd;
+					border-radius:10px;
+				    padding: 10px;
+				    background: #414045;
+				    color: #fff;
+				    cursor: pointer;
+				   
+				}
+				a.phantrang:hover {
+				    background: orange;
+				}
+				.chiatrang{
+					margin-left: 55%;
+				}
+			</style>
+				<br/><br/><br/>
+				<div s class="chiatrang">
+				<?php
+				if(isset($_GET['trang'])){
+					$trang = $_GET['trang'];
+				}else{
+					$trang = 1;
+				}
+				$product_all = $product->get_all_product(); 
+				$product_count = mysqli_num_rows($product_all);
+				$product_button = ceil($product_count/9);
+				$i = 1;
+				
+				for($i=1;$i<=$product_button;$i++){
+					?>
+					<a class="phantrang" <?php if($i==$trang){ echo 'style="background:red"';} ?> style="margin:0 5px;" href="shop.php?trang=<?php echo $i ?>"><?php echo $i ?></a>	
+					<?php
+					
+				}
+				?>
+			</div>
+	</div>
+	
+
+	<!-- //top products -->
+	<div class="mid_slider_w3lsagile">
+		<div class="col-md-3 mid_slider_text">
+			<h5>Nhiều Lựa Chọn Hơn</h5>
+		</div>
+		<div class="col-md-9 mid_slider_info">
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<!-- Indicators -->
+				<ol class="carousel-indicators">
+					<li data-target="#myCarousel" data-slide-to="0" class=""></li>
+					<li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+					<li data-target="#myCarousel" data-slide-to="2" class=""></li>
+					<li data-target="#myCarousel" data-slide-to="3" class=""></li>
+				</ol>
+				<div class="carousel-inner" role="listbox">
+					<div class="item">
+						<div class="row">
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g1.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g2.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g3.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g4.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+						</div>
+					</div>
+					<div class="item active">
+						<div class="row">
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g5.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g6.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g2.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g1.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="row">
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g1.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g2.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g3.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g4.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="row">
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g1.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g2.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g3.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+							<div class="col-md-3 col-sm-3 col-xs-3 slidering">
+								<div class="thumbnail"><img src="images/g4.jpg" alt="Image" style="max-width:100%;"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="fa fa-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+				<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="fa fa-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+				<!-- The Modal -->
+
+			</div>
+		</div>
+
+		<div class="clearfix"> </div>
+	</div>
+	<!-- /newsletter-->
+	<div class="newsletter_w3layouts_agile">
+		<div class="col-sm-6 newsleft">
+			<h3>Đăng Kí Để Nhận Thông Tin Mới Nhất !</h3>
+		</div>
+		<div class="col-sm-6 newsright">
+			<form action="#" method="post">
+				<input type="email" placeholder="Enter your email..." name="email" required="">
+				<input type="submit" value="Gửi">
+			</form>
+		</div>
+
+		<div class="clearfix"></div>
+	</div>
+	<!-- //newsletter-->
+	<!-- footer -->
+	<?php
+		include 'include/footer.php'
+		?>
+	<!-- //footer -->
+<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+	<!-- js -->
+	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+	<!-- //js -->
+	<!-- cart-js -->
+	<!-- <script src="js/minicart.js"></script> -->
+	<script>
+		shoe.render();
+
+		shoe.cart.on('shoe_checkout', function (evt) {
+			var items, len, i;
+
+			if (this.subtotal() > 0) {
+				items = this.items();
+
+				for (i = 0, len = items.length; i < len; i++) {}
+			}
+		});
+	</script>
+	<!-- //cart-js -->
+	<!-- /nav -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<script src="js/classie.js"></script>
+	<script src="js/demo1.js"></script>
+	<!-- //nav -->
+	<!--search-bar-->
+	<script src="js/search.js"></script>
+	<!--//search-bar-->
+	<!-- price range (top products) -->
+	<script src="js/jquery-ui.js"></script>
+	<script>
+		//<![CDATA[ 
+		$(window).load(function () {
+			$("#slider-range").slider({
+				range: true,
+				min: 0,
+				max: 9000,
+				values: [50, 6000],
+				slide: function (event, ui) {
+					$("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+				}
+			});
+			$("#amount").val("$" + $("#slider-range").slider("values", 0) + " - $" + $("#slider-range").slider("values", 1));
+
+		}); //]]>
+	</script>
+	<!-- //price range (top products) -->
+
+	<!-- start-smoth-scrolling -->
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function ($) {
+			$(".scroll").click(function (event) {
+				event.preventDefault();
+				$('html,body').animate({
+					scrollTop: $(this.hash).offset().top
+				}, 1000);
+			});
+		});
+	</script>
+	<!-- //end-smoth-scrolling -->
+	<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+
+
+</body>
+
+</html>
